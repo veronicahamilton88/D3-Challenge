034af79b9408f5b53bd71976b3add4698495590f
Veronica Hamilton <veronica.hamilton88@gmail.com>
2020-10-05 19:41:57 -0400

    File path fix.

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("../../assets/data/data.csv").then(function(healthData) {
+d3.csv("assets/data/data.csv").then(function(healthData) {
 console.log(healthData);
 
     healthData.forEach(function(data) {
